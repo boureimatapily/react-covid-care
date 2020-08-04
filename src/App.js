1bc359a4cf73207abf424dd51224f62dac2d6d11
unknown <boureimatapily1@gmail.com>
2020-08-04 18:00:34 +0000

    MOD add Switch to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./Pages/Home";
+import Department from "./Pages/Department";
 
 function App() {
   return (
-    <div className="App">
-      <button type="button" class="btn btn-primary">welcome</button>
-    </div>
+    <React.Fragment>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/department" component={Department} />
+      </Switch>
+    </React.Fragment>
   );
 }
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,6 +5,7 @@ class Home extends Component {
         return (
             <div>
                 <h1>Welcome to home</h1>
+                <button type="button" class="btn btn-primary">welcome</button>
             </div>
         )
     }
