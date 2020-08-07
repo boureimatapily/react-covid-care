04c32081f10e3d7f297d2bbd0d8e4ce188087aa4
mimi <mimsgh2018@gmail.com>
2020-08-07 04:38:37 +0000

    extracted resusable methods into form component and adde external library to validate forms

--- src/Components/Department/patientForm.jsx
+++ src/Components/Department/patientForm.jsx
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
-import Input from "../../common/input";
+import React from "react";
+import Joi from "joi-browser";
+import Form from "../../common/form";
 
-class PatientForm extends Component {
+class PatientForm extends Form {
   state = {
-    formDetails: {
+    data: {
       fullname: "",
       age: "",
       temperature: "",
@@ -17,40 +18,25 @@ class PatientForm extends Component {
     errors: {},
   };
 
-  validate = () => {
-    const errors = {};
-
-    const { formDetails } = this.state;
-    if (formDetails.fullname.trim() === "")
-      errors.fullname = "fullname is required";
-
-    if (formDetails.report.trim() === "")
-      errors.report = "doctors report is required";
-
-    if (formDetails.status.trim() === "")
-      errors.status = "patients health status is required";
-
-    return Object.keys(errors).length === 0 ? null : errors;
+  schema = {
+    fullname: Joi.string().required().label("Fullname"),
+    age: Joi.string().required().label("Age"),
+    temperature: Joi.string().required().label("Temperature"),
+    height: Joi.string().required().label("Height"),
+    weight: Joi.string().required().label("Weight"),
+    nextofkin: Joi.string().required().label("Next of kin"),
+    contact: Joi.string().required().label("Contact"),
+    status: Joi.string().required().label("Patients status"),
+    report: Joi.string().required().label("Doctors Report"),
   };
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-
-    const errors = this.validate();
-    console.log(errors);
-    this.setState({ errors });
-    if (errors) return;
-
+  doSubmit = () => {
+    // call the server
     console.log("submitted");
   };
 
-  handleChange = ({ currentTarget: input }) => {
-    const formDetails = { ...this.state.formDetails };
-    formDetails[input.name] = input.value;
-    this.setState({ formDetails });
-  };
   render() {
-    const { formDetails, errors } = this.state;
+    const { data, errors } = this.state;
     return (
       <div>
         <h1>patient Details</h1>
@@ -59,7 +45,7 @@ class PatientForm extends Component {
             <div className="form-group col-md-6 mb-3">
               <label htmlFor="fullname">Fullname</label>
               <input
-                value={formDetails.fullname}
+                value={data.fullname}
                 onChange={this.handleChange}
                 type="text"
                 id="fullname"
@@ -67,108 +53,116 @@ class PatientForm extends Component {
                 className="form-control"
                 error={errors.fullname}
               />
-              {errors && (
-                <div className="alert alert-danger"> {errors.fullname} </div>
-              )}
+              {errors && <p className="text-danger"> {errors.fullname} </p>}
             </div>
             <div className="form-group col-md-6 mb-3">
               <label htmlFor="age">Age</label>
               <input
-                value={formDetails.age}
+                value={data.age}
                 onChange={this.handleChange}
                 type="text"
                 id="age"
                 name="age"
                 className="form-control"
               />
+              {errors && <p className="text-danger"> {errors.age} </p>}
             </div>
           </div>
           <div className="row">
             <div className="form-group col-md-4 mb-2">
               <label htmlFor="temperature">temperature</label>
               <input
-                value={formDetails.temperature}
+                value={data.temperature}
                 onChange={this.handleChange}
                 type="text"
                 id="temperature"
                 name="temperature"
                 className="form-control"
               />
+              {errors && <p className="text-danger"> {errors.temperature} </p>}
             </div>
             <div className="form-group col-md-4 mb-2">
               <label htmlFor="weight">weight</label>
               <input
-                value={formDetails.weight}
+                value={data.weight}
                 onChange={this.handleChange}
                 type="text"
                 id="weight"
                 name="weight"
                 className="form-control"
               />
+              {errors && <p className="text-danger"> {errors.weight} </p>}
             </div>
             <div className="form-group col-md-4 mb-2">
               <label htmlFor="height">height</label>
               <input
-                value={formDetails.height}
+                value={data.height}
                 onChange={this.handleChange}
                 type="text"
                 id="height"
                 name="height"
                 className="form-control"
               />
+              {errors && <p className="text-danger"> {errors.height} </p>}
             </div>
           </div>
           <div className="row">
             <div className="col-md-12 mb-6">
               <label htmlFor="status">Patients health status</label>
               <input
-                value={formDetails.status}
+                value={data.status}
                 onChange={this.handleChange}
                 type="text"
                 id="status"
                 name="status"
                 className="form-control"
               />
+              {errors && <p className="text-danger"> {errors.status} </p>}
             </div>
           </div>
           <div className="row">
             <div className="form-group col-md-6 mb-3">
               <label htmlFor="nextofkin">next of kin</label>
               <input
-                value={formDetails.nextofkin}
+                value={data.nextofkin}
                 onChange={this.handleChange}
                 type="text"
                 id="nextofkin"
                 name="nextofkin"
                 className="form-control"
               />
+              {errors && <p className="text-danger"> {errors.nextofkin} </p>}
             </div>
             <div className="form-group col-md-6 mb-3">
               <label htmlFor="contact">contact</label>
               <input
-                value={formDetails.contact}
+                value={data.contact}
                 onChange={this.handleChange}
                 type="text"
                 id="contact"
                 name="contact"
                 className="form-control"
               />
+              {errors && <p className="text-danger"> {errors.contact} </p>}
             </div>
           </div>
           <div className="row">
             <div className="col-md-12 mb-6">
               <label htmlFor="age">Doctors report</label>
               <input
-                value={formDetails.report}
+                value={data.report}
                 onChange={this.handleChange}
                 type="text"
                 id="report"
                 name="report"
                 className="form-control"
               />
+              {errors && <p className="text-danger"> {errors.report} </p>}
             </div>
           </div>
-          <button className="btn-btn-primary mt-5">submit </button>
+          <button disabled={this.validate()} className="btn btn-primary mt-5">
+            submit
+          </button>
         </form>
       </div>
     );
