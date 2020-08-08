0bf2ffcb7cac674f77a8907e84041de006aba1b4
unknown <boureimatapily1@gmail.com>
2020-08-08 02:27:00 +0000

    ADD redux auth Actions

---
+++ src/Redux/Actions/authActions.js
@@ -0,0 +1,66 @@
+import { LOG_IN, LOG_IN_ERR, SIGN_UP, SIGN_UP_ERR, LOG_OUT, LOG_OUT_ERR } from "../Type";
+
+export const register = (creds) => {
+  return (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(creds.email, creds.password)
+      .then(() => {
+        dispatch({ type: SIGN_UP });
+        
+      })
+      .catch((err) => {
+        dispatch({ type: SIGN_UP_ERR }, err);
+      });
+  };
+};
+
+export const login = (creds) => {
+  return (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(creds.email, creds.password)
+      .then(() => {
+        dispatch({ type: LOG_IN });
+      })
+      .catch((err) => {
+        dispatch({ type: LOG_IN_ERR }, err);
+        console.log(err);
+      });
+  };
+};
+
+export const logout = () => {
+  return (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        dispatch({ type: LOG_OUT })
+      })
+      .catch((err) => {
+        dispatch({ type: LOG_OUT_ERR }, err)
+        console.log(err)
+      });
+  };
+};
+
+export const authenticateWithGoogle = (onSuccess) => {
+  return (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((user) => {
+        console.log(user);
+        onSuccess();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+};
