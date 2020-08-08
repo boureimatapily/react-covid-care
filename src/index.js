693b80713b6f3e2dbf224b21c569e379e05ccdb1
unknown <boureimatapily1@gmail.com>
2020-08-08 02:30:08 +0000

    MOD connect redux and firebase

--- src/index.js
+++ src/index.js
@@ -4,28 +4,40 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
-import { ReactReduxFirebaseProvider } from "react-redux-firebase";
-import firebase from "./Config/fbconfig";
-import store from "./Redux/Store";
-import { createFirestoreInstance } from "redux-firestore";
+import { Provider, useSelector } from "react-redux"; // redux provider
+import { ReactReduxFirebaseProvider, isLoaded } from "react-redux-firebase"; // new update from react-redux-firebase v3
+import firebase from "./Config/fbconfig"; // import firebase
+import store from "./Redux/Store"; // import redux store
+import { createFirestoreInstance } from "redux-firestore"; // allow to create firestore intance in our app
 
 // react-redux-firebase config  from react-redux-firebase version 3 to store user profile
-// const rrfConfig = {
-//   userProfile: "users", // where profiles are stored in database
-//   useFirestoreForProfile: true, // Firestore for Profile instead of Realtime DB
-//   // enableClaims: true, // Get custom claims along with the profile
-//   // presence: "presence", // where list of online users is stored in database
-//   sessions: "sessions"
-// };
+const rrfConfig = {
+  userProfile: "users", // firestore collection where users profiles are stored in database
+  useFirestoreForProfile: true, // Firestore for Profile instead of Realtime DB, allow to link firebase and firestore
+  enableClaims: true, // Get custom claims along with the profile, allow to define role in our app
+  presence: "presence", // where list of online users is stored in database
+  sessions: "sessions", // where users sessions are stored in the datebase
+};
 
 // creating react-redux-firebase Props from react-redux-firebase version 3
 const rrfProps = {
   firebase,
-  config: {},
+  config: { rrfConfig },
   dispatch: store.dispatch,
   createFirestoreInstance, // <- needed if using firestore
 };
+
+//Loding animation before data appear
+function AuthIsLoaded({ children }) {
+  const auth = useSelector((state) => state.firebase.auth);
+  if (!isLoaded(auth))
+    return (
+      <div className="text-center">
+        <h1>Loading...</h1>
+      </div>
+    );
+  return children;
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -33,7 +45,9 @@ ReactDOM.render(
       <ReactReduxFirebaseProvider {...rrfProps}>
         {/* ADD BrowserRouter from react-router-dom */}
         <Router>
-          <App />
+          <AuthIsLoaded>
+            <App />
+          </AuthIsLoaded>
         </Router>
       </ReactReduxFirebaseProvider>
     </Provider>
