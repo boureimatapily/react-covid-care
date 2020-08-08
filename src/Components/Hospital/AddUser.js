6b398f6d61d81e13f9c6676ce3b882f58bb4d2dc
unknown <boureimatapily1@gmail.com>
2020-08-08 03:35:45 +0000

    ADD userAdd form

---
+++ src/Components/Hospital/AddUser.js
@@ -0,0 +1,103 @@
+import React from "react";
+
+class AddUser extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      fullname: "",
+      role: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({ [e.target.name]: e.target.value });
+    console.log(this.state);
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const newUser = {
+      email: this.state.email,
+      password: this.state.password,
+      fullname: this.state.fullname,
+      role: this.state.role,
+    };
+  };
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <form>
+              <div className="mb-3">
+                <label for="email" className="form-label">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  name="email"
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                  aria-describedby="emailHelp"
+                />
+              </div>
+              <div className="mb-3">
+                <label for="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  name="password"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                />
+              </div>
+
+              <div className="mb-3">
+                <label for="fullname" className="form-label">
+                  fullname
+                </label>
+                <input
+                  type="fullname"
+                  className="form-control"
+                  id="fullname"
+                  name="fullname"
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                  aria-describedby="fullnameHelp"
+                />
+              </div>
+              <div className="mb-3">
+                <label for="role" className="form-label">
+                  role
+                </label>
+                <input
+                  type="role"
+                  className="form-control"
+                  id="role"
+                  name="role"
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                  aria-describedby="roleHelp"
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                ADD
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AddUser;
