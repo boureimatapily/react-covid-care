01fbd9417a05c426d774b2a38ff0d36579fe193b
unknown <boureimatapily1@gmail.com>
2020-08-14 18:54:14 +0000

    ADD Doctors list by hospital

---
+++ src/Components/Hospital/Doctor/HospitalDoctorsList.js
@@ -0,0 +1,56 @@
+import React from "react";
+import firebase from "../../../Config/fbconfig"
+import SingleDoctor from "./SingleDoctor";
+
+class HospitalDoctorsList extends React.Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      hospitaldoctors:[]
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // This will erase any local state updates!
+    this.setState({ hospitaldoctors:nextProps.listItems})
+  }
+  componentDidMount() {
+        firebase
+          .firestore() //access firestore
+          .collection("users") //access "items" collection
+          .where("status", "==", "Hospital")
+          .onSnapshot(snapshot => {
+            //You can "listen" to a document with the onSnapshot() method.
+            const listItems = snapshot.docs.map(doc => ({
+              //map each document into snapshot
+              id: doc.id, //id and data pushed into items array
+              ...doc.data() //spread operator merges data to id.
+            }));
+            this.setState({ hospitaldoctors:listItems}) //items is equal to listItems
+          });
+  }
+
+  render() {
+    const {  hospitaldoctors } = this.state;
+   
+    console.log(this.state);
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            
+            { hospitaldoctors &&  hospitaldoctors.map((doctor)=>(
+               <div key={doctor.fullname}>
+              <SingleDoctor doctor={doctor} />
+               </div>
+    ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+export default HospitalDoctorsList
