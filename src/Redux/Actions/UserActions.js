726a2b64b1b3ff913fd245f2c5ed0983a9eb9fac
unknown <boureimatapily1@gmail.com>
2020-08-15 01:08:21 +0000

    ADD all days hours

--- src/Redux/Actions/UserActions.js
+++ src/Redux/Actions/UserActions.js
@@ -8,30 +8,28 @@ import {
   UPDATE_USER,
   UPDATE_USER_ERR,
   ADD_DEPARTMENT,
-  ADD_DEPARTMENT_ERR
+  ADD_DEPARTMENT_ERR,
 } from "../Type";
 
-export const checkedPatient = () => {
-  return(dispatch, getState, { getFirebase }) =>{
-    const firestore = getFirebase().firestore()
-    firestore.collection("departments").where("checked", "==", true)
-    .get()
-    .then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-        });
-    })
-    .catch(function(error) {
-        console.log("Error getting documents: ", error);
-    });
-  }
-}
-
-
+// export const checkedPatient = () => {
+//   return(dispatch, getState, { getFirebase }) =>{
+//     const firestore = getFirebase().firestore()
+//     firestore.collection("departments").where("checked", "==", true)
+//     .get()
+//     .then(function(querySnapshot) {
+//         querySnapshot.forEach(function(doc) {
+//             // doc.data() is never undefined for query doc snapshots
+//             console.log(doc.id, " => ", doc.data());
+//         });
+//     })
+//     .catch(function(error) {
+//         console.log("Error getting documents: ", error);
+//     });
+//   }
+// }
 
 //Add new Department
-export const AddDepart= (newDepart) => {
+export const AddDepart = (newDepart) => {
   return (dispatch, getState, { getFirebase }) => {
     const firestore = getFirebase().firestore();
     // const authorId = getState().firebase.auth.uid
@@ -51,32 +49,28 @@ export const AddDepart= (newDepart) => {
   };
 };
 
-
-
-// user authentification actions 
 //Add new user
 export const addPatient = (account) => {
   return (dispatch, getState, { getFirebase }) => {
     const firestore = getFirebase().firestore();
-    const authorId = getState().firebase.auth.uid
+    const authorId = getState().firebase.auth.uid;
     // const id = account.userId;
     firestore
       .collection("patients")
       .add({
         ...account,
-        authorId:authorId,
+        authorId: authorId,
         date: new Date(),
       })
       .then(() => {
-        dispatch({ type: ADD_USER_INFO} , account );
+        dispatch({ type: ADD_USER_INFO }, account);
       })
       .catch((err) => {
-        dispatch({ type: ADD_USER_INFO_ERR},  err);
-          
+        dispatch({ type: ADD_USER_INFO_ERR }, err);
       });
   };
 };
-      //Delete user
+//Delete user
 export const deletePatient = (account) => {
   return (dispatch, getState, { getFirebase }) => {
     const firestore = getFirebase().firestore();
@@ -90,11 +84,12 @@ export const deletePatient = (account) => {
       })
       .catch((err) => {
         dispatch({ type: DELETE_USER_ERR, err });
-        console.log(err)
+        console.log(err);
       });
   };
 };
-    //Update user infos
+
+//Update user infos
 export const updatePatient = (id, account) => {
   return (dispatch, getState, { getFirebase }) => {
     const firestore = getFirebase().firestore();
@@ -117,7 +112,6 @@ export const updatePatient = (id, account) => {
   };
 };
 
-
 //mark patient consultation as don
 export const toggleChecked = (account) => {
   return (dispatch, getState, { getFirebase }) => {
@@ -131,6 +125,32 @@ export const toggleChecked = (account) => {
         {
           ...account,
           checked: !account.checked,
+        },
+        { merge: true }
+      )
+      .then(() => {
+        dispatch({ type: TOGGLE_CHECKED, account });
+      })
+      .catch((err) => {
+        dispatch({ type: TOGGLE_CHECKED_ERR, err });
+      });
+  };
+};
+
+
+//ADD Doctor consult date
+export const addDays= (account) => {
+  return (dispatch, getState, { getFirebase }) => {
+    const firestore = getFirebase().firestore();
+    // const id = account.userId;
+      const id = getState().firebase.auth.uid
+    firestore
+      .collection("users")
+      .doc(id)
+      .set(
+        {
+          ...account,
+          
         },
         { merge: true }
       )
