29c5d2bc38dbd1c05326ad62bedfcd653b22b6b4
unknown <boureimatapily1@gmail.com>
2020-08-15 15:23:28 +0000

    ADD set times

--- src/Components/Hospital/Doctor/Alldays.js
+++ src/Components/Hospital/Doctor/Alldays.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React from "react";
 
-export default function Alldays({day}) {
-    return (
-        <div>
-            <h3>Monday: {day.mondayopen} | {day.mondayclose} </h3>
-            <h3>Tuesday: {day.tuesdayopen} | {day.tuesdayclose}</h3>
-            <h3>Wednesday: {day.wednesdayopen} | {day.wednesdayclose}</h3>
-            <h3>Thursday: {day.thursdayopen} | {day.thursdayclose}</h3>
-            <h3>Friday: {day.fridayopen} | {day.fridayclose}</h3>
-            <h3>Saturday: {day.saturdayopen} | {day.saturdayclose}</h3>
-            <h3>Sunday: {day.sundayopen} | {day.sundayclose}</h3>
+export default function Alldays({ day }) {
+  return (
+    <div className="container">
+        <div className="row">
+            <div className="col">
+            <h3 className="text-center">Availability</h3>
+
+            </div>
         </div>
-    )
+      <div className="row">
+        <div className="col">
+          <h6>
+            Monday: {day.mondayopen} | {day.mondayclose}{" "}
+          </h6>
+          <h6>
+            Tuesday: {day.tuesdayopen} | {day.tuesdayclose}
+          </h6>
+          <h6>
+            Wednesday: {day.wednesdayopen} | {day.wednesdayclose}
+          </h6>
+          <h6>
+            Thursday: {day.thursdayopen} | {day.thursdayclose}
+          </h6>
+        </div>
+        <div className="col">
+          <h6>
+            Friday: {day.fridayopen} | {day.fridayclose}
+          </h6>
+          <h6>
+            Saturday: {day.saturdayopen} | {day.saturdayclose}
+          </h6>
+          <h6>
+            Sunday: {day.sundayopen} | {day.sundayclose}
+          </h6>
+        </div>
+      </div>
+    </div>
+  );
 }
