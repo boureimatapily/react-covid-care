ce512c1a745adee1a2cc13b678547f2fc79fd5a9
unknown <boureimatapily1@gmail.com>
2020-08-16 18:43:57 +0000

    FIX fetching bug

--- src/Components/Hospital/Doctor/HomeDoctorsList.js
+++ src/Components/Hospital/Doctor/HomeDoctorsList.js
@@ -40,7 +40,7 @@ class HomeDoctorsList extends React.Component{
           <div className="col d-flex flex-row flex-wrap">
             
             {homedoctors && homedoctors.map((doctor)=>(
-               <div key={doctor.fullname} className="col-4 mb-3">
+               <div key={doctor.fullname} className="col-sm-12 col-xs-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4 mb-2">
               <SingleDoctor doctor={doctor}  />
                </div>
     ))}
