3d191acfddc5d51290710a047e53362b6aca1263
Boureima TAPILY <56594883+boureimatapily@users.noreply.github.com>
2023-02-03 15:09:41 +0000

    firebase compta

--- src/Config/fbconfig.js
+++ src/Config/fbconfig.js
@@ -1,10 +1,10 @@
-import * as firebase from "firebase/app";
+import * as firebase from "firebase/compat/app";
 // Add the Firebase services that you want to use
-import "firebase/firestore";
-import "firebase/auth";
-import "firebase/database";
-import "firebase/storage";
-import "firebase/analytics";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import "firebase/compat/database";
+import "firebase/compat/storage";
+import "firebase/compat/analytics";
 
 var firebaseConfig = { // firebase config 
   apiKey: "AIzaSyB9sIGOPZPiI4aRh3n5YEu7wRA79lBn0eY",
